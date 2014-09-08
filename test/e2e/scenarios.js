8ab15023f3c34e7416d5c50d78a7772455054722
phyllis.treve <phyllistreve@yahoo.com>
2014-09-08 23:02:31 +0000

    fifth commit

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -7,9 +7,29 @@ describe('PhoneCat App', function() {
   beforeEach(function() {
     browser.get('app/index.html');
   });
-it ('should filter the phone list as user types into the search box', function(){
-	var phoneList = element.all(by.repeater('phone in phones'));
+it ('should be possible to control phone order via the drop down search box', function(){
+	var phoneNameColumn = element.all(by.repeater('phone in phones').column('{{phone.name}}'));
 	var query = element(by.model('query'));
+	
+	function getNames() {
+	return phoneNameColumn.map(function(elm){
+		return elm.getText();
+		});
+	}
+	query.sendKeys('tablet');
+	
+	expect (getNames()).toEqual([
+	"Motorola XOOM\u2122 with Wi-Fi",
+	"MOTOROLA XOOM\u2122"
+	]);
+	
+	element(by.model('orderProp')).element(by.css('option[value="name"]')).click();
+	
+	expect (getNames()).toEqual([
+	"MOTOROLA XOOM\u2122",
+	"Motorola XOOM\u2122 with Wi-Fi"
+	]);
+	});
 	expect (phoneList.count()).toBe(3);
 	query.sendKeys('nexus');
 	expect(phoneList.count()).toBe(1);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,13 +2,23 @@
 
 /* jasmine specs for controllers go here */
 
+describe ('PhoneCat controllers', function() {
 describe('PhoneListCtrl', function() {
+var scope, ctrl;
 beforeEach(module('phonecatApp'));
+
+beforeEach(inject(function($controller){
+	scope ={};
+		ctrl =$controller('PhoneListCtrl', {scope:scope});
+		}));
   it('should create "phones" model with 3 phones',
 function() {
-var scope = {},
-ctrl = new PhoneListCtrl(scope);
-expect(scope.phones.length).toBe(3);
+	expect(scope.phones.length).toBe(3);
   });
 
+  it ('should set the default value of orderProp model',
+  function() {
+  expect (scope.orderProp).toBe('age');
+  });
+});
 });
