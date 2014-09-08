26345c9342f906dd65c7a19dc5e4ca9f6de515eb
phyllis.treve <phyllistreve@yahoo.com>
2014-09-08 23:28:32 +0000

    sixth commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,18 +3,12 @@
 /* Controllers */
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope){
-$scope.phones =[
-{'name': 'Nexus S',
-'snippet': 'Fast just got faster with Nexus S.',
-'age': 1},
-{'name': 'Motorolla XOOM™ with Wi-Fi',
-'snippet': 'The Next, Next Generation tablet.',
-'age': 2},
-{'name': 'MOTOROLLA XOOM™',
-'snippet': 'The Next, Next Generation tablet.',
-'age': 3}
-];
-$scope.orderProp ='age';
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http',
+function($scope, $http){
+$http.get('phones/phones.json').success(function(data){
+$scope.phones = data;
 });
+
+$scope.orderProp ='age';
+}]);
 
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -4,17 +4,23 @@
 
 describe ('PhoneCat controllers', function() {
 describe('PhoneListCtrl', function() {
-var scope, ctrl;
+var scope, ctrl, $httpBackend;
 beforeEach(module('phonecatApp'));
 
-beforeEach(inject(function($controller){
-	scope ={};
+beforeEach(inject(function(_$httpBackend_, $rootScope, $controller){
+	$httpBackend =_$httpBackend_;
+	$httpBackend.expectGET('phones/phones.json').
+		respond([{name: 'Nexus S'}, {name: 'Motorola DROID'}]);
+		scope =$rootScope.$new();
 		ctrl =$controller('PhoneListCtrl', {scope:scope});
 		}));
-  it('should create "phones" model with 3 phones',
+  it('should create "phones" model with 2 phones fetched from xhr',
 function() {
-	expect(scope.phones.length).toBe(3);
-  });
+	expect(scope.phones).toBeUndefined();
+	$httpBackend.flush();
+	expect(scope.phones).toEqual([{name:'Nexus S'},
+									{name: 'Motorola DROID'}]);
+	});
 
   it ('should set the default value of orderProp model',
   function() {
