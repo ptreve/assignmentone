bb45f8d086a450755836f49fea73433ae262d39b
phyllis.treve <phyllistreve@yahoo.com>
2014-09-08 21:25:02 +0000

    third commit

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,10 +2,13 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function() {
-
-  it("should do something", function() {
-
+describe('PhoneListCtrl', function() {
+beforeEach(module('phonecatApp'));
+  it('should create "phones" model with 3 phones',
+function() {
+var scope = {},
+ctrl = new PhoneListCtrl(scope);
+expect(scope.phones.length).toBe(3);
   });
 
 });
